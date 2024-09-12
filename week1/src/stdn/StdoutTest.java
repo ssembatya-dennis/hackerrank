d6b52fc0f101f7bef00a80a326925a379e399513
dennis <ssembatyadennis25@gmail.com>
2024-09-12 14:19:59 +0300

    soln(Standard Output): Display an integer, double and a string value entered in the system

---
+++ week1/src/stdn/StdoutTest.java
@@ -0,0 +1,54 @@
+package stdn;
+import java.util.Scanner;
+
+/*
+ * In this challenge, you must read an integer, a double, and a String from stdin, 
+ * then print the values according to the instructions in the Output Format section below. 
+ * To make the problem a little easier, a portion of the code is provided for you in the editor.
+
+Note: We recommend completing Java Stdin and Stdout I before attempting this challenge.
+
+Input Format
+
+There are three lines of input:
+
+The first line contains an integer.
+The second line contains a double.
+The third line contains a String.
+Output Format
+
+There are three lines of output:
+
+On the first line, print String: followed by the unaltered String read from stdin.
+On the second line, print Double: followed by the unaltered double read from stdin.
+On the third line, print Int: followed by the unaltered integer read from stdin.
+To make the problem easier, a portion of the code is already provided in the editor.
+
+Note: If you use the nextLine() method immediately following the nextInt() method,
+ recall that nextInt() reads integer tokens; because of this, the last newline character for that 
+ line of integer input is still queued in the input buffer and the next nextLine() will be reading 
+ the remainder of the integer line (which is empty).
+ */
+
+public class StdoutTest {
+    public static void main(String[] args) {
+        // Instance the Scanner class to have access to all the methods that be used to read
+        // Input entered into the system 
+        Scanner scannerobject = new Scanner(System.in);
+
+        // read the interger value and store it in a variable intergerValue
+        int intergerValue = scannerobject.nextInt();
+
+        // read the double value and store it in a variable doubleValue
+        double doubleValue = scannerobject.nextDouble();
+
+        // remove space character(s) after the doubleValue to ensure to store a fresh string. 
+        scannerobject.nextLine();
+        String s = scannerobject.nextLine();
+        
+        // display output for the input stored in the integer, double and string variables
+        System.out.println("String: " + s);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Int: " + intergerValue);
+    }
+}
